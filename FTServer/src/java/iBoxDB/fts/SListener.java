2a677e12eea2ff1deadeb610a99b0dca183e2e77
iBoxDB <iboxdb@users.noreply.github.com>
2016-03-24 01:19:47 -0700

    FTS 52

--- FTServer/src/java/iBoxDB/fts/SListener.java
+++ FTServer/src/java/iBoxDB/fts/SListener.java
@@ -25,7 +25,7 @@ public class SListener implements ServletContextListener {
         }
 
         if (path == null) {
-            path = System.getProperty("user.home") + "/ftsdata51/";
+            path = System.getProperty("user.home") + "/ftsdata52/";
             new File(path).mkdirs();
 
             String tmpPath = sce.getServletContext().getRealPath("/")
