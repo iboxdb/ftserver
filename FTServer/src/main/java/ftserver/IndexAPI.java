f0e986efface47fd0d327b39762c21a779e3d404
Bruce <iboxdb@users.noreply.github.com>
2021-05-28 23:24:43 -0400

    page2

--- FTServer/src/main/java/ftserver/IndexAPI.java
+++ FTServer/src/main/java/ftserver/IndexAPI.java
@@ -7,14 +7,14 @@ import static ftserver.App.*;
 import java.text.NumberFormat;
 
 public class IndexAPI {
-    
+
     final static Engine ENGINE = new Engine();
-    
+
     private static class StartIdParam {
         //andBox,orBox, ids...
 
         public long[] startId;
-        
+
         public StartIdParam(long[] id) {
             if (id.length == 1) {
                 startId = new long[]{App.Indices.size() - 1, -1, id[0]};
@@ -22,7 +22,7 @@ public class IndexAPI {
                 startId = id;
             }
         }
-        
+
         public boolean isAnd() {
             if (startId[0] >= 0) {
                 if (startId[2] >= 0) {
@@ -34,17 +34,17 @@ public class IndexAPI {
             }
             return false;
         }
-        
+
         protected ArrayList<StringBuilder> ToOrCondition(String name) {
             String orName = new String(ENGINE.sUtil.clear(name));
             orName = orName.replaceAll("\"", " ").trim();
-            
+
             ArrayList<StringBuilder> ors = new ArrayList<StringBuilder>();
             ors.add(new StringBuilder());
             for (int i = 0; i < orName.length(); i++) {
                 char c = orName.charAt(i);
                 StringBuilder last = ors.get(ors.size() - 1);
-                
+
                 if (c == ' ') {
                     if (last.length() > 0) {
                         ors.add(new StringBuilder());
@@ -70,13 +70,13 @@ public class IndexAPI {
                     }
                 }
             }
-            
+
             ors.add(0, null); //and box
             ors.add(1, null); //or box
             ors.add(2, null); //and startId
             //full search
             ors.add(3, new StringBuilder(name));
-            
+
             if (startId.length != ors.size()) {
                 startId = new long[ors.size()];
                 startId[0] = -1;
@@ -86,16 +86,16 @@ public class IndexAPI {
                     startId[i] = Long.MAX_VALUE;
                 }
             }
-            
+
             if (ors.size() > 16 || stringEqual(ors.get(3).toString(), ors.get(4).toString())) {
                 for (int i = 0; i < startId.length; i++) {
                     startId[i] = -1;
                 }
             }
-            
+
             return ors;
         }
-        
+
         public boolean isOr() {
             if (startId[1] >= 0) {
                 for (int i = 3; i < startId.length; i++) {
@@ -112,14 +112,14 @@ public class IndexAPI {
             return false;
         }
     }
-    
+
     public static long[] Search(List<PageText> outputPages,
             String name, long[] t_startId, long pageCount) {
         name = name.trim();
         if (name.length() > 150) {
             return new long[]{-1, -1, -1};
         }
-        
+
         StartIdParam startId = new StartIdParam(t_startId);
         long beginTime = System.currentTimeMillis();
         long maxTime = 1000 * 2;
@@ -161,16 +161,16 @@ public class IndexAPI {
         }
         return startId.startId;
     }
-    
+
     private static long SearchAnd(AutoBox auto, List<PageText> pages,
             String name, long startId, long pageCount) {
         name = name.trim();
-        
+
         try (Box box = auto.cube()) {
             for (KeyWord kw : ENGINE.searchDistinct(box, name, startId, pageCount)) {
                 pageCount--;
                 startId = kw.I - 1;
-                
+
                 long id = kw.I;
                 PageText pt = PageText.fromId(id);
                 Page p = getPage(pt.textOrder);
@@ -181,21 +181,21 @@ public class IndexAPI {
                     pt.isAndSearch = true;
                     pages.add(pt);
                 } else {
-                    App.log("Old Version Page Not Showing: " + p.url); 
+                    App.log("Old Version Page Not Showing: " + p.url + " (" + name + ") AND");
                 }
             }
-            
+
             return pageCount == 0 ? startId : -1;
         }
     }
-    
+
     private static void SearchOr(AutoBox auto, List<PageText> outputPages,
             ArrayList<StringBuilder> ors, long[] startId, long pageCount) {
-        
+
         try (Box box = auto.cube()) {
-            
+
             Iterator<KeyWord>[] iters = new Iterator[ors.size()];
-            
+
             for (int i = 0; i < ors.size(); i++) {
                 StringBuilder sbkw = ors.get(i);
                 if (sbkw == null || sbkw.length() < 2) {
@@ -206,13 +206,13 @@ public class IndexAPI {
                 long subCount = pageCount * 10;
                 iters[i] = ENGINE.searchDistinct(box, sbkw.toString(), startId[i], subCount).iterator();
             }
-            
+
             int orStartPos = 3;
             KeyWord[] kws = new KeyWord[iters.length];
-            
+
             int mPos = maxPos(startId);
             while (mPos >= orStartPos) {
-                
+
                 for (int i = orStartPos; i < iters.length; i++) {
                     if (kws[i] == null) {
                         if (iters[i] != null && iters[i].hasNext()) {
@@ -224,18 +224,18 @@ public class IndexAPI {
                         }
                     }
                 }
-                
+
                 if (outputPages.size() >= pageCount) {
                     break;
                 }
-                
+
                 mPos = maxPos(startId);
-                
+
                 if (mPos > orStartPos) {
                     KeyWord kw = kws[mPos];
-                    
+
                     long id = kw.I;
-                    
+
                     PageText pt = PageText.fromId(id);
                     Page p = getPage(pt.textOrder);
                     if (p.show) {
@@ -244,22 +244,24 @@ public class IndexAPI {
                         pt.page = p;
                         pt.isAndSearch = false;
                         outputPages.add(pt);
+                    } else {
+                        App.log("Old Version Page Not Showing: " + p.url + " OR ");
                     }
                 }
-                
+
                 long maxId = startId[mPos];
                 for (int i = orStartPos; i < startId.length; i++) {
                     if (startId[i] == maxId) {
                         kws[i] = null;
                     }
                 }
-                
+
             }
-            
+
         }
-        
+
     }
-    
+
     private static int maxPos(long[] ids) {
         int orStartPos = 3;
         orStartPos--;
@@ -270,7 +272,7 @@ public class IndexAPI {
         }
         return orStartPos;
     }
-    
+
     private static boolean stringEqual(String a, String b) {
         if (a.equals(b)) {
             return true;
@@ -283,11 +285,11 @@ public class IndexAPI {
         }
         return false;
     }
-    
+
     public static Page getPage(long textOrder) {
         return App.Item.get(Page.class, "Page", textOrder);
     }
-    
+
     public static long addPage(Page page) {
         /*
         Page oldPage = GetOldPage(page.url);
@@ -310,9 +312,9 @@ public class IndexAPI {
             return -1L;
         }
     }
-    
+
     public static boolean addPageIndex(final long textOrder) {
-        
+
         Page page = getPage(textOrder);
         if (page == null) {
             return false;
@@ -324,17 +326,17 @@ public class IndexAPI {
         }
         ArrayList<PageText> ptlist = Html.getDefaultTexts(page);
         int count = 0;
-        
+
         for (PageText pt : ptlist) {
             count++;
             addPageTextIndex(pt, count == ptlist.size() ? 0 : HuggersMemory);
         }
         return true;
     }
-    
+
     public static void addPageTextIndex(PageText pt, long huggers) {
         try (Box box = App.Index.cube()) {
-            
+
             ENGINE.indexText(box, pt.id(), pt.indexedText(), false,
                     new Runnable() {
                 @Override
@@ -346,11 +348,11 @@ public class IndexAPI {
             log("MEM:  " + NumberFormat.getInstance().format(cr.GetMemoryLength(box)));
         }
     }
-    
+
     public static void DisableOldPage(String url) {
         try (Box box = App.Item.cube()) {
             ArrayList<Page> page = new ArrayList<Page>();
-            
+
             for (Page p : box.select(Page.class, "from Page where url==? limit 1,10", url)) {
                 if (!p.show) {
                     break;
@@ -364,7 +366,7 @@ public class IndexAPI {
             box.commit().Assert();
         }
     }
-    
+
     public static Page GetOldPage(String url) {
         ArrayList<Page> pages = App.Item.select(Page.class, "from Page where url==? limit 0,1", url);
         if (pages.size() > 0) {
