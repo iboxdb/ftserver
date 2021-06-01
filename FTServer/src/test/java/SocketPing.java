1320a17fe7734990a1548d1afaf668ecf642bc94
Bruce <iboxdb@users.noreply.github.com>
2021-06-01 09:01:30 -0400

    ip

--- FTServer/src/test/java/SocketPing.java
+++ FTServer/src/test/java/SocketPing.java
@@ -1,13 +1,28 @@
 
 import java.io.IOException;
 import java.net.*;
+import java.util.Enumeration;
 
 public class SocketPing {
 
     public static void main(String[] arg) throws IOException {
-        
+
         System.out.println("Ping");
         System.out.println("systemctl stop firewalld");
+
+        System.out.println("45ae:65cd".matches(".*[abcdef].*"));
+
+        for (Enumeration<NetworkInterface> en = NetworkInterface
+                .getNetworkInterfaces(); en.hasMoreElements();) {
+            NetworkInterface intf = en.nextElement();
+            for (Enumeration<InetAddress> enumIpAddr = intf
+                    .getInetAddresses(); enumIpAddr.hasMoreElements();) {
+                InetAddress inetAddress = enumIpAddr.nextElement();
+                String ip = inetAddress.getHostAddress();
+                System.out.println(ip);
+            }
+        }
+
         ServerSocket ss = new ServerSocket(9000);
         while (true) {
             Socket s = ss.accept();
